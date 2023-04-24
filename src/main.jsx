8776f2eba2ba1320e1a8faa231f24d33248c1cf2
Rasec Camacho <rasec.camacho19@gmail.com>
2023-04-23 17:46:28 -0700

    react router installation

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/counterWithCustomHook'
@@ -14,10 +16,13 @@ import './index.css'
 // import { CallBackHook } from './06-memos/CallBackHook'
 // import { Padre } from './07-tarea-memo/Padre'
 // import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp'
+// import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <TodoApp />
-  // </React.StrictMode>,
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
